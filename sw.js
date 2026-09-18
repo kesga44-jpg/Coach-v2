262b0a0e6523cea67ff45d350cada583f1f4f051
kesga44-jpg <kesga44@gmail.com>
2026-09-18 15:15:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='football-coach-v2.2.4';
-const CORE=['./','./index.html','./style.css?v=2.2.4','./app.js?v=2.2.4','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='football-coach-v2.2.5';
+const CORE=['./','./index.html','./style.css?v=2.2.5','./app.js?v=2.2.5','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
